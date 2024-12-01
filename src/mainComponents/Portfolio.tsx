56962025206c80a3cd287189dc4f05182d158010
NeoEx <draja.acc@gmail.com>
2024-12-01 10:45:09 -0300

    feat-solved problems with ts

--- src/mainComponents/Portfolio.tsx
+++ src/mainComponents/Portfolio.tsx
@@ -22,8 +22,8 @@ const Portfolio = () => {
         <div className="container mx-auto text-center py-32 w-full">
           <h2 className="text-5xl font-bold mb-8 text-lime-400 font-sten">Portfolio</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center">
-            {cards.map((itens, _) => (
-              <Cards cardTitle={itens.title} cardBodyText={itens.body} cardButtonText={itens.button} />
+            {cards.map((itens, index) => (
+              <Cards key={index} cardTitle={itens.title} cardBodyText={itens.body} cardButtonText={itens.button} />
             ))}
           </div>
         </div>
