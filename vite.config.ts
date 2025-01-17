e48965393669531508f9cdc08ec74901ca263205
NeoEx <draja.acc@gmail.com>
2025-01-17 12:23:48 -0300

    error on pre build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  // assetsInclude: ["**/*.json"],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
